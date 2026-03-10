cf8b85a1470982ad9dcfdfb3e08b540a721fc174
Malcolm Roosdahl <malcoroo@gmail.com>
2026-03-09 18:21:42 -0700

    Added autocomplete to Create Group Form.

--- src/createGroup.js
+++ src/createGroup.js
@@ -1,42 +1,40 @@
 import { v4 as uuidv4 } from "uuid";
 import { db } from "/src/firebaseConfig.js";
 import { doc, setDoc } from "firebase/firestore";
+import { getCurrentUser } from "./authentication";
 
 const form = document.getElementById("createGroupForm");
-
+//Get form data and then use the create group function to store form variables
 form.addEventListener("submit", function (event) {
   event.preventDefault();
   const formData = new FormData(form);
 
   const name = formData.get("group-name");
   const location = formData.get("destinations-list");
+  const currentUser = getCurrentUser();
   const maxBuddies = formData.get("maxBuddies");
   const tags = document.getElementsByName("tags[]");
   const method = formData.get("travel-method");
   const visibility = formData.get("visibility");
-  console.log(name);
-  console.log(location);
-  console.log(maxBuddies);
   const tagsArray = [];
+  const membersArray = [];
+  const groupStatus = true;
   for (var i = 0; i < tags.length; i++) {
     if (tags[i].checked == true) {
       tagsArray.push(tags[i].value);
     }
   }
-  console.log(tagsArray);
-  console.log(method);
-  console.log(visibility);
 
   CreateGroup(
     name.toString(),
-    tagsArray.toString(),
-    "testID",
+    tagsArray,
+    currentUser.toString(),
     location.toString(),
-    method.toString(),
+    method,
     maxBuddies,
-    "testID, testID, testID",
+    membersArray,
     visibility.toString(),
-    "open",
+    groupStatus,
   );
 });
 
@@ -53,7 +51,7 @@ export async function CreateGroup(
 ) {
   //Generates a random group ID
   let randomGID = uuidv4();
-  console.log(randomGID);
+
   const group = {
     groupName: name,
     tags: tags,
